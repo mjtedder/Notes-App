00fafe9a5643ed26fc318eb544594748f20f19af
mjtedder <mjtedder@gmail.com>
2019-04-24 09:29:10 -0600

    Create initial local storage values
    I have set the default value of the note title and note body to display
    when the page is loaded.  Next, I will create a function that saves what
    the user types using localStorage.
    
    I also moved the check and trash icons to the jumbotron for now.  Was
    having an issue where it would overwrite those with the default value of
    localStorage.  I will address this in a future update.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,15 @@
 // Execute after pages loads
 $(document).ready(function () {
 
+// Setting global variables
+const noteTitle = $('.card-header')
+const noteContent = $('.card-body')
+const defaultTitle = 'What\'s on your mind today?'
+const defaultContent = 'Let\'s write about it!'
 
-
+// Use localStorage to display defualt values
+noteTitle.text(localStorage['title'] || defaultTitle)
+noteContent.text(localStorage['body'] || defaultContent)
 
 
 
