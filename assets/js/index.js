ad495e97ec227d5b3530d58a5bc35169de8757f9
mjtedder <mjtedder@gmail.com>
2019-04-27 10:07:44 -0600

    Save user input
    When the user writes a note, the corresponding local storage value is
    replaced with what the user inputs.  On reload, these values replace the
    default value.
    
    Next, I need to give the user the ability to click the plus icon and a
    new note card is created.

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,11 +7,30 @@ const noteContent = $('.card-body')
 const defaultTitle = 'What\'s on your mind today?'
 const defaultContent = 'Let\'s write about it!'
 
-// Use localStorage to display defualt values
-noteTitle.text(localStorage['title'] || defaultTitle)
-noteContent.text(localStorage['body'] || defaultContent)
+
+// Use localStorage to display values
+// localStorage.setItem('title', defaultTitle)
+noteTitle.text(localStorage.getItem('title'))
+// localStorage.setItem('content', defaultContent)
+noteContent.text(localStorage.getItem('content'))
+
+console.log(localStorage)
 
 
+// Create note function
+ let save = () => {
+    let title = $('.card-header').text()
+    localStorage.setItem('title', title)
+    let content = $('.card-body').text()
+    localStorage.setItem('content', content)
+}
+
+// Retrieve localStorage items
+localStorage.getItem('test')
+
+// Call function everytime the user makes a keystroke
+document.onkeyup = () => save()
+document.onkeyup = () => save()
 
 
 
